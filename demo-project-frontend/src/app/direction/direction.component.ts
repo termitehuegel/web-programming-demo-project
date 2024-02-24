5942bcf64d434a1b8f254e3b23cd6943041ad1ef
Philip Berner <philipberner.berlin@gmail.com>
2024-02-24 14:18:45 +0100

    initialised sub site components

---
+++ demo-project-frontend/src/app/direction/direction.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-direction',
+  standalone: true,
+  imports: [],
+  templateUrl: './direction.component.html',
+  styleUrl: './direction.component.scss'
+})
+export class DirectionComponent {
+
+}
