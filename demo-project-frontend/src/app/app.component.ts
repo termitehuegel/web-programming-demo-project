58a7573de7cf1b57fe94a8625550f68f8ec04b07
Philip Berner <philipberner.berlin@gmail.com>
2024-02-24 14:17:37 +0100

    added navbar and footer with links

--- demo-project-frontend/src/app/app.component.ts
+++ demo-project-frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {MatToolbar, MatToolbarRow} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarRow, MatToolbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- demo-project-frontend/src/app/app.routes.ts
+++ demo-project-frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import {MenuComponent} from "./menu/menu.component";
+import {HomeComponent} from "./home/home.component";
+import {ReservationComponent} from "./reservation/reservation.component";
+import {DeliveryComponent} from "./delivery/delivery.component";
+import {ImprintComponent} from "./imprint/imprint.component";
+import {ContactComponent} from "./contact/contact.component";
+import {DirectionComponent} from "./direction/direction.component";
+import {PrivacyComponent} from "./privacy/privacy.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'menu', component: MenuComponent},
+    {path: 'menu', component: MenuComponent},
+    {path: 'reservation', component: ReservationComponent},
+    {path: 'delivery', component: DeliveryComponent},
+    {path: 'direction', component: DirectionComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'imprint', component: ImprintComponent},
+    {path: 'privacy-policy', component: PrivacyComponent},
+];
