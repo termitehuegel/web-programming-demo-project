5452b8c6243552180413ec2562c3698dce048571
Philip Berner <philipberner.berlin@gmail.com>
2024-03-13 11:19:41 +0100

    - added images - reworked home a bit

--- demo-project-frontend/src/app/app.config.ts
+++ demo-project-frontend/src/app/app.config.ts
@@ -4,7 +4,9 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {provideNativeDateAdapter} from "@angular/material/core";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(HttpClientModule),]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(HttpClientModule), provideAnimations(), provideNativeDateAdapter()]
 };
